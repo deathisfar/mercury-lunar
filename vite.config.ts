61ca5003cc685b92d395a9025d3cf0d5e2b33a24
Amin Nazari <47194722+deathisfar@users.noreply.github.com>
2025-08-11 15:00:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,25 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      react(),
-      // only add componentTagger in development mode
-      mode === "development" && componentTagger(),
-    ].filter(Boolean),
-    base: "/mercury-lunar/",
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
 
-    // optional dev server config (only used locally)
-    server: {
-      host: "::",
-      port: 8080,
-    },
+  base: "/mercury-lunar/",
 
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+  // optional dev server config (only used locally)
+  server: {
+    host: "::",
+    port: 8080,
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  };
-});
+  },
+}));
